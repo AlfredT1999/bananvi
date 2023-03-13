d7f1a66e8376ddfe4b5ad4a22fe309cb1d54df1e
People <alfredtapia1999@outlook.com>
2023-03-13 17:58:00 -0600

    contact updates

--- src/app/navbar/contact/contact.component.ts
+++ src/app/navbar/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -6,5 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent {
+  email = new FormControl('', [
+    Validators.required,
+    Validators.minLength(2)
+  ]);
+  tema = new FormControl('', [
+    Validators.required,
+    Validators.minLength(2)
+  ]);
+  mensaje = new FormControl('', [
+    Validators.required,
+    Validators.minLength(2)
+  ]);
 
+  contactForm = new FormGroup({
+    mensaje: this.mensaje,
+    tema: this.tema,
+    email: this.email
+  }, []);
+
+  constructor() {
+    
+  }
+
+  contact() {
+
+  }
 }
